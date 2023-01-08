ea43ca051dc19617ff52428c4690051d89e498d6
Usui Dubs <121833646+usui-dubs@users.noreply.github.com>
2023-01-08 20:37:51 +0100

    Add files via upload

---
+++ Loops.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char S[4];
+	int i, len;
+	
+	do
+	{
+		printf("Write a word less than 4 characters: : ");
+		gets(S);
+	} while ( strlen(S) >= 4) ;
+		
+	for (i=0; S[i] != '\0'; i++)
+	//{
+	    printf("\ni = %d", i);
+	//}
+ 
+ printf("\nwe exit the loop and");
+ printf("\nthe string(mot) length is %d", i);
+
+	return 0;
+} 
